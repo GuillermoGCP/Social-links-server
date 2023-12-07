dbad70040c90287a5adc58563f574a3774fcefa7
Guillermo C Porto <guillermocporto@gmail.com>
2023-12-07 18:32:40 +0100

    Crear endPoint create Link

--- src/server.js
+++ src/server.js
@@ -3,6 +3,23 @@ import "dotenv/config";
 const { PORT } = process.env;
 const app = express();
 
+app.use(express.json());
+
+app.post("/links", async (req, res) => {
+  const loggedUserId = req.auth.id;
+  const { url, title, description } = req.body;
+  const [insertId] = await pool.query(
+    "INSERT INTO links (url, title, description, ownerId)VALUES(?, ?, ?, ?)",
+    [url, title, description, loggedUserId]
+  );
+  res.send({
+    status: "ok",
+    data: {
+      id: insertId,
+    },
+  });
+});
+
 //Server:
 app.listen(PORT, () => {
   console.log(`Servidor activo en el puerto ${PORT}`);
