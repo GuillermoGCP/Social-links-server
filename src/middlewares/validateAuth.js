4ca518d598d960693af94530a022760ec0bc7960
Guillermo C Porto <guillermocporto@gmail.com>
2024-01-16 11:57:48 +0100

    eliminar validateAuth condicional

--- src/middlewares/validateAuth.js
+++ src/middlewares/validateAuth.js
@@ -1,38 +1,34 @@
 import jwt from "jsonwebtoken";
 import { generateError } from "../utils/index.js";
 
-const validateAuth = (params) => {
-  return (req, res, next) => {
-    try {
-      const { authorization } = req.headers;
+const validateAuth = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
 
-      if (!authorization) {
-        if (params?.optional) return next();
-
-        generateError("Se requiere autorización", 401);
-      }
-
-      const [tokenType, token] = authorization.split(" ");
-
-      if (tokenType !== "Bearer") {
-        generateError("El token debe ser de tipo 'Bearer'", 400);
-      }
-
-      let tokenPayload;
-
-      try {
-        tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
-      } catch (error) {
-        generateError("El token es inválido", 400);
-      }
-
-      req.auth = tokenPayload;
-
-      next();
-    } catch (error) {
-      next(error);
+    if (!authorization) {
+      generateError("Se requiere autorización", 401);
     }
-  };
+
+    const [tokenType, token] = authorization.split(" ");
+
+    if (tokenType !== "Bearer") {
+      generateError("El token debe ser de tipo 'Bearer'", 400);
+    }
+
+    let tokenPayload;
+
+    try {
+      tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      generateError("El token es inválido", 400);
+    }
+
+    req.auth = tokenPayload;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default validateAuth;
