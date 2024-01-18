dab2f83865f02941c275b99d1d4ba4b3520b3e0c
Ruth Villa <ruthvv@gmail.com>
2024-01-18 14:19:32 +0100

    Cambio seeAllLinks mayuscula

--- src/models/links/seeAllLinks.js
+++ src/models/links/seeAllLinks.js
@@ -5,7 +5,7 @@ const seeAllLinks = async (loggedUserId) => {
   await useDb();
 
   const [links] = await pool.query(
-    "SELECT l.*, u.name, u.profilePicture, IFNULL(AVG(v.rating), 0) rating, BIT_OR(v.userId = ?) votedByLoggedUser FROM links l LEFT JOIN userlink v ON l.id = v.linkId LEFT JOIN users u ON l.ownerId = u.id GROUP BY l.id;",
+    "SELECT l.*, u.name, u.profilePicture, IFNULL(AVG(v.rating), 0) rating, BIT_OR(v.userId = ?) votedByLoggedUser FROM links l LEFT JOIN userLink v ON l.id = v.linkId LEFT JOIN users u ON l.ownerId = u.id GROUP BY l.id;",
     [loggedUserId]
   );
 
