1f8286cb8a668fa88e99f9e368db6420fe52334b
Guillermo C Porto <guillermocporto@gmail.com>
2024-01-22 16:29:40 +0100

    añadir id a la ruta de getProfile para recogerlo después desde el front

--- src/controllers/users/getProfile.js
+++ src/controllers/users/getProfile.js
@@ -1,32 +1,30 @@
-import {getOwnLinks} from "../../models/links/index.js";
+import { getOwnLinks } from "../../models/links/index.js";
 import { selectUserById } from "../../models/users/index.js";
 
-
-
-const getProfile = async (req, res,next)=>{
-    try {
-        const loggedUserId=req.auth.id;
-        const user= await selectUserById(loggedUserId);
-        let links= await getOwnLinks(loggedUserId)
-        if (links.length <= 0) {
-            links="No has publicado ningun link"
-        }
-        res.send({
-            status:"ok",
-            data: {
-                user: user.name,
-                email: user.email,
-                biography:user.biography,
-                profilePicture: user.profilePicture
-            },
-            ownLinks:{
-                links
-            }
-        })
-        
-    } catch (error) {
-        next(error);
+const getProfile = async (req, res, next) => {
+  try {
+    const loggedUserId = req.auth.id;
+    const user = await selectUserById(loggedUserId);
+    let links = await getOwnLinks(loggedUserId);
+    if (links.length <= 0) {
+      links = "No has publicado ningun link";
     }
-}
+    res.send({
+      status: "ok",
+      data: {
+        id: user.id,
+        user: user.name,
+        email: user.email,
+        biography: user.biography,
+        profilePicture: user.profilePicture,
+      },
+      ownLinks: {
+        links,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export default getProfile
+export default getProfile;
