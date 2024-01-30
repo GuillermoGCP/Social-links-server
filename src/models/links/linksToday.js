a2b27e1216cc466366ff7fccab450ca870311754
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-01-30 19:05:59 +0100

    traer todos los datos del modelo seeLinksToday

--- src/models/links/linksToday.js
+++ src/models/links/linksToday.js
@@ -1,14 +1,14 @@
 import pool from "../../db/getPool.js";
 import useDb from "../../db/useDb.js";
 
-const seeLinksToday = async () => {
+const seeLinksToday = async (loggedUserId) => {
   const todayDate = new Date();
   await useDb();
   const [links] = await pool.query(
-    "SELECT l.*, u.name, u.profilePicture FROM links l JOIN users u ON l.ownerId = u.id WHERE DATE(l.createdAt) = DATE(?);",
-    [todayDate]
+    "SELECT l.*, u.name, u.profilePicture,GROUP_CONCAT(v.userId) AS voterUserIds, IFNULL(AVG(v.rating), 0) rating, GROUP_CONCAT(v.rating) AS individualRatings, BIT_OR(v.userId = ?) votedByLoggedUser FROM links l LEFT JOIN userLink v ON l.id = v.linkId LEFT JOIN users u ON l.ownerId = u.id WHERE DATE(l.createdAt) = DATE(?) GROUP BY l.id ;",
+    [loggedUserId, todayDate]
   );
-  console.log(links);
+
   return links;
 };
 
